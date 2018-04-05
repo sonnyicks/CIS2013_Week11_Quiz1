cd17b9769a5b8c083d0ae4a9a49841117d2796dc
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-04-04 19:30:31 -0500

    quiz in progress

--- CIS2013_Week11_Quiz1.cpp
+++ CIS2013_Week11_Quiz1.cpp
@@ -2,29 +2,46 @@
 using namespace std;
 
 typedef int* iptr;
-
 int main(){
 	
 	int rows;
 	int computers;
-	int total;
+	bool entering = true;
 	
 	cout << "How many rows?: ";
 	cin >> rows;
-	cout << "How many computers on each row?: ";
+	cout << "How many computers on each?: ";
 	cin >> computers;
-	total = (rows*computers);
 	
 	iptr *t = new iptr[rows];
 	for (int i=0; i<rows; i++){
 		t[i] = new int[computers];
 	}
+	
 	for (int i=0; i<rows; i++){
 		for (int j=0; j<computers; j++){
+			t[i][j] = 0;
 			cout << "empty ";
 		}
 		cout << endl;
 	}
 	
+	int x = 0;
+	int y = 0;
+	int grade = 0;
+	while (entering){
+	cout << "what is the grade? :";
+	cin >> grade;
+	cout << "What row and computer gets it?: ";
+	cin >> x >> y;
+	t[x][y] = grade;
+	for (int i=0; i<rows; i++){
+		for (int j=0; j<computers; j++){
+			if (t[i][j]==0){cout << "empty ";}
+			else{cout << t[i-1][j-1];}
+	}
+
 	return 0;
+}
+	}
 }
